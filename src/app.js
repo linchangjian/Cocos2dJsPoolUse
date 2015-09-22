500b313772149a8c2c24e1c60c0fca79357addb0
林长健 <linchangjianting@163.com>
2015-09-22 16:27:15 +0800

    对象缓冲池使用
    测试用例

---
+++ src/app.js
@@ -0,0 +1,119 @@
+
+var ButtonClose = cc.Sprite.extend({
+
+    ctor:function(){
+        this._super(res.CloseSelected_png);
+        this.x = Math.random()*400 + 100;
+        ButtonClose.btnCloseId++;
+        this.btnCloseId = ButtonClose.btnCloseId;
+        this.scheduleUpdate();
+    },
+    update:function(){
+        this.y++;
+        if(this.y > 300){
+            this.unscheduleUpdate();
+            this.removeFromParent(true);
+            cc.pool.putInPool(this);
+            cc.log("remove le "+ this.btnCloseId);
+        }
+    },
+
+    reuse:function(){
+       this.y = 0;
+       this.scheduleUpdate();
+    }
+
+});
+
+ButtonClose.btnCloseId = 0;
+
+ButtonClose.reCreate = function(){
+    //如果池里有，就从池里拿，没有就新创建一个
+    if(cc.pool.hasObject(ButtonClose)){
+        return cc.pool.getFromPool(ButtonClose);
+    }else{
+        return new ButtonClose();
+    }
+}
+
+var HelloWorldLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+
+        // add a "close" icon to exit the progress. it's an autorelease object
+        var closeItem = new cc.MenuItemImage(
+            res.CloseNormal_png,
+            res.CloseSelected_png,
+            function () {
+                cc.log("Menu is clicked!");
+            }, this);
+        closeItem.attr({
+            x: size.width - 20,
+            y: 20,
+            anchorX: 0.5,
+            anchorY: 0.5
+        });
+
+        var menu = new cc.Menu(closeItem);
+        menu.x = 0;
+        menu.y = 0;
+        this.addChild(menu, 1);
+        setInterval(function(){
+         var btnClose = ButtonClose.reCreate();
+                this.addChild(btnClose);
+        }.bind(this),300);
+
+        /////////////////////////////
+        // 3. add your codes below...
+        // add a label shows "Hello World"
+        // create and initialize a label
+        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
+        // position the label on the center of the screen
+        helloLabel.x = size.width / 2;
+        helloLabel.y = 0;
+        // add the label as a child to this layer
+        this.addChild(helloLabel, 5);
+//
+//        // add "HelloWorld" splash screen"
+//        this.sprite = new cc.Sprite(res.HelloWorld_png);
+//        this.sprite.attr({
+//            x: size.width / 2,
+//            y: size.height / 2,
+//            scale: 0.5,
+//            rotation: 180
+//        });
+//        this.addChild(this.sprite, 0);
+//
+//        this.sprite.runAction(
+//            cc.sequence(
+//                cc.rotateTo(2, 0),
+//                cc.scaleTo(2, 1, 1)
+//            )
+//        );
+//        helloLabel.runAction(
+//            cc.spawn(
+//                cc.moveBy(2.5, cc.p(0, size.height - 40)),
+//                cc.tintTo(2.5,255,125,0)
+//            )
+//        );
+        return true;
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
